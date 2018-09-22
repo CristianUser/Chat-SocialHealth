a95a5d5117f3e4662d04d6091ce5924a7034050e
CristianUser <theimaginario@hotmail.com>
2018-09-22 18:24:43 -0400

    version 1.0 completa

--- request/getUserList1.php
+++ request/getUserList1.php
@@ -3,10 +3,10 @@
     $id=$_GET["id"];
     ///consultamos a la base
     $id=$_GET["id"];
-	$where="WHERE rp.ID_Cliente = $id";
-    $sql = "SELECT * FROM usuario usr INNER JOIN datos_profesional dp 
-    ON usr.id_usuario = dp.ID_Usuario and usr.id_usuario INNER JOIN 
-    r_paciente rp on rp.ID_Profesional = dp.ID_Usuario $where";
+	$where="WHERE ID_Profesional = $id";
+    $sql = "SELECT * FROM usuario usr INNER JOIN datos_cliente dp 
+    ON usr.id_usuario = dp.ID_Usuario and usr.id_usuario in 
+    (SELECT ID_Cliente FROM r_paciente $where)";
     $ejecutar = $mysqli->query($sql);
     
     while($fila = $ejecutar->fetch_array()) : 
@@ -19,7 +19,6 @@
         // Format the image SRC:  data:{mime};base64,{data};
         $perfil = 'data: '.mime_content_type($img_file).';base64,'.$imgData;
 ?>
-<?php echo $fila['id_usuario']?>
     <li value="<?php echo $fila['id_usuario']?>" class="list-group-item"><div class="incoming_msg_img"> <img src="<?php echo $perfil?>" alt=""> 
     </div><p><?php echo $fila['nombre']," ",$fila['apellido']?></p></li>
 	
